5e4d306671670526f053c6daa6692be1a08a1a4d
Jason.Wu <wujpkimo@gmail.com>
2021-02-26 18:20:14 +0800

    加入專案檔案。

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <ul>
+                <asp:Repeater ID="rptr" runat="server">
+                    <ItemTemplate>
+                        <li><%# DataBinder.Eval(Container.DataItem, "Name") %>,<%# Eval("Tel") %></li>
+                    </ItemTemplate>
+                </asp:Repeater>
+            </ul>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<(string name, string tel)> navs = new List<(string name, string tel)>(){
+                ("str","str"),("str1","str1"),("str2","str2") };
+            List<MyClass> navs1 = new List<MyClass>(){
+                new MyClass(){Name="<div>test</div>",Tel="str" },
+                new MyClass(){Name="str1",Tel="str1" },
+                new MyClass(){Name="str2",Tel="str2" }};
+            String[] myStringArray = new String[] { "one", "two", "three" };
+
+            rptr.DataSource = navs1;
+            rptr.DataBind();
+        }
+
+        private class MyClass
+        {
+            public string Name { get; set; }
+            public string Tel { get; set; }
+        }
+    }
+}
